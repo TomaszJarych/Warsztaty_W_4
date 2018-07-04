3b97d8b7184cda4830634880390ffd9b9f1a1ad0
Tomasz Jarych <tjarych@gmail.com>
2018-07-04 17:53:36 +0200

    Warsztaty 4

--- APP/js/app.js
+++ APP/js/app.js
@@ -1,57 +1,26 @@
 $(document).ready(function () {
     $.ajax({
-        url: "http://localhost:8282/books/"
-    }).done(function (booksJSON) {
-        var books = [];
-        $.each(booksJSON, function () {
-            books.push(this)
-        });
-        var tbody = $("#tbody");
-        for (var book of books) {
-            var newTr = $("<tr>");
-            var newTdID = $("<td>")
-            newTdID.text(book.id);
-            var newTdISBN = $("<td>")
-            newTdISBN.text(book.isbn);
-            var newTdAuthor = $("<td>")
-            newTdAuthor.text(book.author)
-            var newTdTitle = $("<td>")
-            newTdTitle.text(book.title);
-            var button = $("<button>")
-            button.text("Więcej informacji")
-            newTdTitle.append(button);
-            var newTdPublisher = $("<td>")
-            newTdPublisher.text(book.publisher)
-            var newTdType = $("<td>")
-            newTdType.text(book.type);
-            var newDiv = $("<div>", {
-                id: book.id
-            });
-            newTr.append(newTdID).append(newTdISBN).append(newTdAuthor)
-                .append(newTdTitle).append(newTdPublisher)
-                .append(newTdType);
-            tbody.append(newTr).append(newDiv);
+        url: "http://localhost:8282/books",
+        type: "GET",
+        dataType: "json"
+    }).done(function (booksList) {
+        if (!booksList) {
+            return;
         }
-    }); // tutaj dodać fail() i always();
+        var booksListDiv = $("#booksList");
+        for (var book of booksList) {
+            var element = "";
+            var element = "<div class = \"bookEntry\"><h1>" + book["title"] + "</h1>";
+            element += "<button id=\"bookID" + book["id"] + "\">Details </button>";
+            element += "<div class=\"showDetails\" id=\"bookDetailsID" + book["id"] + 
+            "\"></div>";
+            element += "</div>";
+            booksListDiv.append(element);
+            $("#bookID" + book["id"]).on("click", {id: book["id"]}, function(){
+                console.log("Pyk i działa");
+            });
+        }
 
-})
-var tbody = $("#tbody");
-tbody.on("click", "button", function (event) {
-    var target = $(event.target);
-    var div = target.parent().parent().next("div");
-    var bookId = (div.attr("id"));
-    $.ajax({
-        url: "http://localhost:8282/books/"+bookId
-    }).done(function (booksJSON) {
-        
-        var additionalInfo = "ID to "+ booksJSON.id+ " ISBN to: "+booksJSON.isbn +" Autor to "+ booksJSON.author+ 
-        " Tytuł to "+booksJSON.title+ " Wydawca to "+booksJSON.publisher+ " Gatunek : "+ booksJSON.type;
-        var newP = $("<p>").text(additionalInfo)
-        div.append(newP)
-        
-    });
-        
-
-
+    })
 
 });
